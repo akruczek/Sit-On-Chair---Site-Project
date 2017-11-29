e6898c79bd12e1462fa6de3182ed37c871960057
Adam Kruczek <Adam.Krucz1@gmail.com>
2017-11-29 18:08:44 +0100

    Added form and footer

--- js/script.js
+++ js/script.js
@@ -5,31 +5,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // var moreButton = document.querySelector(".moreButton");
 
   unwrapMenuOpenButton.addEventListener("mouseover", function(event) {
+    // unwrapMenu.style.animation = "showMenu 1s";
     unwrapMenu.style.display = "block";
   });
 
   unwrapMenuOpenButton.addEventListener("mouseout", function(event) {
+    // unwrapMenu.style.animation = "showMenu 1s reverse";
     unwrapMenu.style.display = "none";
   });
 
   unwrapMenu.addEventListener("mouseover", function(event) {
+    // unwrapMenu.style.animation = "showMenu 1s";
     this.style.display = "block";
   });
 
   unwrapMenu.addEventListener("mouseout", function(event) {
-    this.style.display = "none";
+    // unwrapMenu.style.animation = "showMenu 1s reverse";
+    unwrapMenu.style.display = "none";
   });
 
   logo.addEventListener("mouseover", function(event) {
-    logo.querySelector("a").classList.add("changeColor");
-    logo.querySelector("a:nth-of-type(2)").classList.add("changeColorReverse");
-    logo.querySelector("a:last-of-type").classList.add("changeColor");
+    // logo.querySelector("a").classList.add("changeColor");
+    // logo.querySelector("a:nth-of-type(2)").classList.add("changeColorReverse");
+    // logo.querySelector("a:last-of-type").classList.add("changeColor");
   });
 
   logo.addEventListener("mouseout", function(event) {
-    logo.querySelector("a").classList.remove("changeColor");
-    logo.querySelector("a:nth-of-type(2)").classList.remove("changeColorReverse");
-    logo.querySelector("a:last-of-type").classList.remove("changeColor");
+    // logo.querySelector("a").classList.remove("changeColor");
+    // logo.querySelector("a:nth-of-type(2)").classList.remove("changeColorReverse");
+    // logo.querySelector("a:last-of-type").classList.remove("changeColor");
   });
 
 });
